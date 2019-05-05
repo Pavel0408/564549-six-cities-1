250e9a05f7a78443ca7672e567455c64322efc97
Pavel <pavel0408@gmail.com>
2019-05-05 14:04:50 +0300

    карточки отрисовываются из массива

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import {MainScreen} from "../main-screen/main-screen";
 
-export const App = () => {
-  return <MainScreen/>;
+export const App = (props) => {
+  const {cardTitles} = props;
+  console.log(cardTitles);
+
+  return <MainScreen cardTitles={cardTitles} />;
 };
