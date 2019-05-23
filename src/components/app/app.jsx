039cd7d670177a7b8205b762e9aa82fb4044d750
Pavel <pavel0408@gmail.com>
2019-05-23 13:44:06 +0300

    подключил хранилище, обернул App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import {MainScreen} from "../main-screen/main-screen";
+import connect from "react-redux/es/connect/connect";
 
 export const App = (props) => {
   const {offers} = props;
@@ -14,3 +15,8 @@ export const App = (props) => {
 App.propTypes = {
   offers: PropTypes.array.isRequired
 };
+
+const mapStateToProps = (state, ownProps) => Object.assign({},
+    ownProps);
+
+export const AppConnected = connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import {App} from "./components/app/app";
+import {AppConnected} from "./components/app/app";
 import {offers} from "./mocks/offers";
 import {reducer} from "./reducer";
 
+/* eslint-disable no-underscore-dangle */
 const init = () => {
-  ReactDOM.render(
-      <App
-        offers={offers}
-      />,
-      document.querySelector(`#root`)
+  const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  /* eslint-enable */
+  ReactDOM.render(<Provider store={store}>
+    <AppConnected
+      offers={offers}
+    />
+  </Provider>,
+  document.querySelector(`#root`)
   );
 };
+
 
 init();
 reducer(undefined, {
