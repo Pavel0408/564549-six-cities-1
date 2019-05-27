07ea76572a69bdda858c47a2f45d065e09326e71
Pavel <pavel0408@gmail.com>
2019-05-27 11:54:01 +0300

    убрал из mapStateToProps ownProps

--- src/components/app/app-connected.jsx
+++ src/components/app/app-connected.jsx
@@ -3,16 +3,15 @@ import connect from "react-redux/es/connect/connect";
 import {ActionCreator} from "../../action-creator";
 import {App} from "./app";
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   const cities = [...new Set(state.offers.map((offer) => offer.city))];
   const offers = state.offers.filter((offer) => offer.city === state.cityName);
 
-  return Object.assign({},
-      ownProps, {
-        cityName: state.cityName,
-        offers,
-        cities
-      });
+  return Object.assign({}, {
+    cityName: state.cityName,
+    offers,
+    cities
+  });
 };
 
 const mapDispatchToProps = (dispatch) => {
