a9a287a016f8ef19c0e450d2938841267e4b19fa
Pavel <pavel0408@gmail.com>
2019-06-01 15:36:53 +0300

    добавил тесты на ActionCreator

--- src/action-creator.test.js
+++ src/action-creator.test.js
@@ -1,5 +1,6 @@
 import {ActionCreator} from "./action-creator";
 import {offers} from "./mocks/offers";
+import {ActionType} from "./action-type";
 
 describe(`ActionCreator is correct`, () => {
   it(`Return correctly action on city changing`, () => {
@@ -16,6 +17,39 @@ describe(`ActionCreator is correct`, () => {
       type: `CHANGE_OFFERS`,
       payload: {
         offers
+      }
+    });
+  });
+  it(`Return correctly action on fetchOffers`, () => {
+    expect(ActionCreator.fetchOffers()).toEqual({
+      type: ActionType.FETCH_OFFERS,
+      payload: {
+        isLoading: true,
+        isFailed: false,
+        isLoaded: false,
+        error: null
+      }
+    });
+  });
+  it(`Return correctly action on fetchOffersReceived`, () => {
+    expect(ActionCreator.fetchOffersReceived()).toEqual({
+      type: ActionType.FETCH_OFFERS_RECEIVED,
+      payload: {
+        isLoading: false,
+        isFailed: false,
+        isLoaded: true,
+        error: null
+      }
+    });
+  });
+  it(`Return correctly action on fetchOffersFailed`, () => {
+    expect(ActionCreator.fetchOffersFailed(`error`)).toEqual({
+      type: ActionType.FETCH_OFFERS_FAILED,
+      payload: {
+        isLoading: false,
+        isLoaded: false,
+        isFailed: true,
+        error: `error`
       }
     });
   });
