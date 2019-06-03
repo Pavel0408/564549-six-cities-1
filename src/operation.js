af1984fbdcb1f2effe2f6e37b25dd7871155beb1
Pavel <pavel0408@gmail.com>
2019-06-03 18:47:12 +0300

    убрал лишние вызовы dispatch

--- src/operation.js
+++ src/operation.js
@@ -4,16 +4,13 @@ import {ServerPath} from "./constants/server-path";
 
 export const Operation = {
   loadOffers: () => (dispatch, getState, api) => {
-    dispatch(ActionCreator.loadingOffers(true));
+    dispatch(ActionCreator.loadingOffers());
     return api.get(ServerPath.hotels)
       .then(parseServerResponseOffers)
       .then((offers) => {
         dispatch(ActionCreator.fetchOffersReceived(offers));
-        dispatch(ActionCreator.loadingOffers(false));
-        dispatch(ActionCreator.fetchOffersFailed(null));
       }).catch((e) => {
         dispatch(ActionCreator.fetchOffersFailed(e));
-        dispatch(ActionCreator.loadingOffers(false));
       });
   },
 };
