ac4e93528479698b7c1fee2e4fdaccfc0d3af3b6
Pavel <pavel0408@gmail.com>
2019-06-16 15:32:44 +0300

    отображаются три ближайших объявления

--- src/utils.js
+++ src/utils.js
@@ -27,3 +27,27 @@ export const getRandomCoordinates = (coordinates) => {
 
   return coordinates.map(getRandomCoordinate);
 };
+
+export const toRad = (x) => {
+  return (x * Math.PI) / 180;
+};
+
+export const getDistanceFromCoords = ({coordinateFirst, coordinateSecond}) => {
+  const EARTH_RADIUS = 6371; // km;
+
+  coordinateFirst.map((coord) => parseInt(coord, 10));
+  coordinateSecond.map((coord) => parseInt(coord, 10));
+  const [latFirst, longFirst] = coordinateFirst;
+  const [latSecond, longSecond] = coordinateSecond;
+
+  const x1 = latSecond - latFirst;
+  const dLat = toRad(x1);
+  const x2 = longSecond - longFirst;
+  const dLon = toRad(x2);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(latFirst)) * Math.cos(toRad(latSecond)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return EARTH_RADIUS * c;
+};
