ffd2223c800a762d7b1acf57182a2f003507ba89
Pavel <pavel0408@gmail.com>
2019-06-17 17:07:56 +0300

    select открывается и закрывается

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -8,6 +8,7 @@ import {WithLeafletMap} from "../with-leaflet-map/with-leaflet-map";
 import {CitiesList} from "../cities-list/cities-list";
 import {offersPropTypes} from "../../prop-types/offers-prop-types";
 import {WithActiveItem} from "../../hocs/with-active-item";
+import {Sort} from "../sort/sort";
 
 
 export const MainScreen = (props) => {
@@ -94,36 +95,8 @@ export const MainScreen = (props) => {
             <b className="places__found">
               {offers.length} places to stay in {cityName}
             </b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex="0">
-              Popular
-                <svg
-                  className="places__sorting-arrow"
-                  width="7"
-                  height="4"
-                >
-                  <use xlinkHref="#icon-arrow-select"/>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom">
-                <li
-                  className="places__option places__option--active"
-                  tabIndex="0"
-                >
-                  Popular
-                </li>
-                <li className="places__option" tabIndex="0">
-                  Price: low to high
-                </li>
-                <li className="places__option" tabIndex="0">
-                  Price: high to low
-                </li>
-                <li className="places__option" tabIndex="0">
-                  Top rated first
-                </li>
-              </ul>
-            </form>
+            <Sort/>
+
             <div className="cities__places-list places__list tabs__content" style={{overflow: `auto`, height: `calc(100vh - 350px)`}}>
               {isLoading && <h3>Offers is loading</h3>}
               {error && <h3>Download failed {error.message}</h3>}
