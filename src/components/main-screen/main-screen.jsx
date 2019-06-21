792cf0141e369a72bcb6b946bf23fac7ca44a764
Pavel <pavel0408@gmail.com>
2019-06-21 14:10:08 +0300

    перенёс отрисовку блока с оферами и картой в отдельный компонент

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -8,6 +8,7 @@ import {WithLeafletMap} from "../with-leaflet-map/with-leaflet-map";
 import {CitiesList} from "../cities-list/cities-list";
 import {offersPropTypes} from "../../prop-types/offers-prop-types";
 import {Sort} from "../sort/sort";
+import {CitiesPlaces} from "../cities-places/cities-places";
 
 export const MainScreen = (props) => {
   const {offers, cityName, cityClickHandler, cities, isLoading, error, user,
@@ -84,35 +85,7 @@ export const MainScreen = (props) => {
         cityName={cityName}
       />
       <div className="cities__places-wrapper">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">
-              {offers.length} places to stay in {cityName}
-            </b>
-            <Sort
-              changeSort={changeSort}
-              sort={sort}
-            />
-
-            <div className="cities__places-list places__list tabs__content" style={{overflow: `auto`, height: `calc(100vh - 350px)`}}>
-              {isLoading && <h3>Offers is loading</h3>}
-              {error && <h3>Download failed {error.message}</h3>}
-              <OffersList offers={offers}
-                changeActiveOffer={changeActiveOffer}
-                sort={sort}
-                changeActivePinOffer={changeActivePinOffer}
-                fetchReviews={fetchReviews}
-              />
-            </div>
-          </section>
-          <div className="cities__right-section">
-            <section className="cities__map map" style={{backgroundImage: `none`, height: `calc(100vh - 200px)`}}>
-              <WithLeafletMap
-                render={(data) => <OffersMap mapMethods={data} offers={offers} cityName={cityName} activePinOffer={activePinOffer}/>}/>
-            </section>
-          </div>
-        </div>
+        <CitiesPlaces {...props}/>
       </div>
     </main>
   </React.Fragment>;
