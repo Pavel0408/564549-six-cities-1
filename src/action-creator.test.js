7fb8e164c97604d9561c39d6f1d1e5bc5c1dc4d4
Pavel <pavel0408@gmail.com>
2019-06-29 13:49:07 +0300

    добавил тесты на action creator

--- src/action-creator.test.js
+++ src/action-creator.test.js
@@ -1,9 +1,9 @@
 import {
   authorization, authorizationFailed,
-  changeActiveCity,
+  changeActiveCity, changeActivePinOffer, changeSort, fetchFavoriteFailed, fetchFavoriteReceived,
   fetchOffersFailed,
-  fetchOffersReceived,
-  loadingOffers, setActiveOffer
+  fetchOffersReceived, fetchReviewsFailed, fetchReviewsSuccess, loadingFavorite,
+  loadingOffers, sendingReviews, sendingReviewsError, setActiveOffer
 } from "./action-creator";
 import {ActionType} from "./action-type";
 
@@ -26,14 +26,36 @@ describe(`ActionCreator is correct`, () => {
       }
     });
   });
+  it(`Return correctly action on favorite fetched`, () => {
+    const offers = [];
+    expect(fetchFavoriteReceived(offers)).toEqual({
+      type: ActionType.FAVORITE_RECEIVED,
+      payload: {
+        favoriteOffers: []
+      }
+    });
+  });
   it(`Return correctly action on fetchingOffers`, () => {
     expect(loadingOffers(true)).toEqual({
       type: ActionType.FETCH_OFFERS_LOADING
     });
   });
+  it(`Return correctly action on fetching favorite Offers`, () => {
+    expect(loadingFavorite()).toEqual({
+      type: ActionType.FAVORITE_LOADING
+    });
+  });
   it(`Return correctly action on fetchOffersFailed`, () => {
     expect(fetchOffersFailed(`error`)).toEqual({
       type: ActionType.FETCH_OFFERS_FAILED,
+      payload: {
+        error: `error`
+      }
+    });
+  });
+  it(`Return correctly action on fetch fetch favorite Offers Failed`, () => {
+    expect(fetchFavoriteFailed(`error`)).toEqual({
+      type: ActionType.FETCH_FAVORITE_FAILED,
       payload: {
         error: `error`
       }
@@ -60,6 +82,51 @@ describe(`ActionCreator is correct`, () => {
       type: ActionType.ACTIVE_OFFER,
       payload: {
         activeOffer: `newOffer`
+      }
+    });
+  });
+  it(`Return correctly action on fetch Reviews Success`, () => {
+    expect(fetchReviewsSuccess(`reviews`)).toEqual({
+      type: ActionType.FETCH_REVIEWS_SUCCESS,
+      payload: {
+        reviews: `reviews`
+      }
+    });
+  });
+  it(`Return correctly action on fetch Reviews Failed`, () => {
+    expect(fetchReviewsFailed(`error`)).toEqual({
+      type: ActionType.FETCH_REVIEWS_FAILED,
+      payload: {
+        error: `error`
+      }
+    });
+  });
+  it(`Return correctly action on change sort`, () => {
+    expect(changeSort(`newSort`)).toEqual({
+      type: ActionType.CHANGE_SORT,
+      payload: {
+        sort: `newSort`
+      }
+    });
+  });
+  it(`Return correctly action on change active pin offer`, () => {
+    expect(changeActivePinOffer(`newOffer`)).toEqual({
+      type: ActionType.ACTIVE_PIN_OFFER_CHANGE,
+      payload: {
+        activePinOffer: `newOffer`
+      }
+    });
+  });
+  it(`Return correctly action on sending reviews `, () => {
+    expect(sendingReviews()).toEqual({
+      type: ActionType.SENDING_REVIEWS
+    });
+  });
+  it(`Return correctly action on sending reviews error`, () => {
+    expect(sendingReviewsError(`sendingError`)).toEqual({
+      type: ActionType.SENDING_REVIEWS_ERROR,
+      payload: {
+        sendingError: `sendingError`
       }
     });
   });
