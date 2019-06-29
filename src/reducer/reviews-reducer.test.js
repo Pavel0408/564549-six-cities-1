28692ecfe17154dd3332a84794e89511eb8a5437
Pavel <pavel0408@gmail.com>
2019-06-29 15:41:07 +0300

    добавил тесты на reviewsReducer

---
+++ src/reducer/reviews-reducer.test.js
@@ -0,0 +1,81 @@
+import {ActionType} from "../action-type";
+
+import {reviewsReducer} from "./reviews-reducer";
+
+describe(`reviewsReducer is correct`, () => {
+  it(`returns initial state without parameters`, () => {
+    expect(reviewsReducer(undefined, {})).toEqual({
+      reviews: [],
+      error: null,
+      isSending: false,
+      sendingError: null
+    });
+  });
+  it(`returns correctly state on incorrect action`, () => {
+    const successAuthorizationAction = {
+      type: ActionType.ACTIVE_OFFER,
+      payload: {
+        user: `something`
+      }
+    };
+    const state = {
+      reviews: [],
+      error: null,
+      isSending: false,
+      sendingError: null
+    };
+    expect(reviewsReducer(state, successAuthorizationAction)).toEqual({
+      reviews: [],
+      error: null,
+      isSending: false,
+      sendingError: null
+    });
+  });
+  it(`returns correctly state when fetch reviews success`, () => {
+    const fetchReviewsSuccessAction = {
+      type: ActionType.FETCH_REVIEWS_SUCCESS,
+      payload: {
+        reviews: []
+      }
+    };
+    expect(reviewsReducer({}, fetchReviewsSuccessAction)).toEqual({
+      error: null,
+      sendingError: null,
+      isSending: false,
+      reviews: []
+    });
+  });
+  it(`returns correctly state when fetch reviews failed`, () => {
+    const fetchReviewsFailedAction = {
+      type: ActionType.FETCH_REVIEWS_FAILED,
+      payload: {
+        error: `error`
+      }
+    };
+    expect(reviewsReducer({}, fetchReviewsFailedAction)).toEqual({
+      error: `error`,
+      reviews: []
+    });
+  });
+  it(`returns correctly state when reviews sending`, () => {
+    const sendingReviewsAction = {
+      type: ActionType.SENDING_REVIEWS,
+    };
+    expect(reviewsReducer({}, sendingReviewsAction)).toEqual({
+      isSending: true,
+      sendingError: null
+    });
+  });
+  it(`returns correctly state when sending reviews was failed`, () => {
+    const sendingReviewsFailedAction = {
+      type: ActionType.SENDING_REVIEWS_ERROR,
+      payload: {
+        sendingError: `error`
+      }
+    };
+    expect(reviewsReducer({}, sendingReviewsFailedAction)).toEqual({
+      isSending: false,
+      sendingError: `error`
+    });
+  });
+});
