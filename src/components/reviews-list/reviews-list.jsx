75c6ea931225810721be689f5c19fe2ab9dc56fd
Pavel <pavel0408@gmail.com>
2019-06-30 13:50:09 +0300

    исправил опечатку

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -15,10 +15,10 @@ export const ReviewsList = (props) => {
   return <section className="property__reviews reviews">
     <h2 className="reviews__title">Reviews · <span className="reviews__amount">{reviews.length}</span></h2>
     <ul className="reviews__list">
-      {reviews.map((rewview) => {
+      {reviews.map((review) => {
         return <Review
-          key={props.activeOffer.id.toString() + rewview.id}
-          comment={rewview}
+          key={props.activeOffer.id.toString() + review.id}
+          comment={review}
         />;
       })}
     </ul>
